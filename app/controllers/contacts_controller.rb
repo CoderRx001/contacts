33fef2c7056be8cb14f79dfb9b9c40307b6c8f1d
CoderRx001 <carrillo964@gmail.com>
2017-05-10 17:41:49 -0500

    new

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,52 @@
+class ContactsController < ApplicationController
+  # def one_contact
+  #   @contact = Contact.first
+  #   render 'one_contact.html.erb'
+  # end
+
+  # def many_contacts
+  #   @contacts = Contact.all 
+  #   render 'many_contacts.html.erb'
+  # end
+
+  def index
+    @contacts = Contact.all
+  end
+
+  def new
+  
+  end
+  def create
+    contact = Contact.new(
+                          first_name: params[:first_name],
+                          last_name: params[:last_name],
+                          email: params[:email],
+                          phone: params[:phone]
+                          )
+    contact.save
+  end
+
+  def show
+    contact_id = params[:id]
+    @contacts = Contact.find_by(id: recipe_id)
+  end
+
+  def edit
+        @contacts = Contact.find(params[:id])
+  end
+  def update
+    contacts = Contact.find(params[:id])
+    contact.assign_attributes(
+                          first_name: params[:first_name],
+                          last_name: params[:last_name],
+                          email: params[:email],
+                          phone: params[:phone]
+                          )
+    contact.save
+  end
+
+  def destroy
+    contact_id = find(params[:id])
+    contact.destroy
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # get '/contact' => 'contacts#one_contact'
+    # get '/contacts' => 'contacts#many_contacts'
+
+    # get '/contacts' => 'contactss#one_contact_action'
+
+    get '/' => 'contacts#index'
+
+    get '/contacts' => 'contacts#index'
+
+    get '/contacts/new' => 'contacts#new' #these 2 are pairs
+    post '/contacts' => 'contact#create'
+
+    get '/contacts/:id' => 'contacts#show'
+
+    get '/contacts/:id/edit' => 'contacts#edit' #these 2 are pairs
+    patch '/contacts/:id' => 'contacts#update'
+
+    delete '/contacts/:id' => 'contacts#destroy'
+
+end
